404731a513650a3b2b09b8fe73466771e9bfb730
Andrew Peacock <todoville@gmail.com>
2018-04-16 15:20:25 -0700

    some html scaffolding complete, getting js underway. buttons made, 3 variable images in html, beginning to make event listeners

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+console.log('link workin bruvvvvvvv');
+
+function Image(name, url) {
+this.name = name,
+this.url = url,
+timesShown = 0,
+clicks = 0;
+}
+
+
+var bag = new Image('bag', 'img/bag.jpg,url');
+var banana = new Image('banana', 'img/banana.jpg.url');
+var bathroom = new Image('bathroom', 'img/bathroom.jpg.url');
+var boots = new Image('Boots', 'img/boots.jpg.url');
+var breakfast = new Image('Breakfast', 'img/bathroom.jpg.url');
+var 
+
+boots;
+breakfast;
