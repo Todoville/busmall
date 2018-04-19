c29a459a86687058cf62af2dba5416ccb1ef6430
Andrew Peacock <todoville@gmail.com>
2018-04-18 17:24:25 -0700

    oh man it's been too long since I committed uhhhhhhh I made a chart and did some debugs sorry for this terrible commit message

--- app.js
+++ app.js
@@ -10,19 +10,21 @@ MerchImage.imgPlaceholderOne = document.getElementById('firstImage');
 MerchImage.imgPlaceholderTwo = document.getElementById('secondImage');
 MerchImage.imgPlaceholderThree = document.getElementById('thirdImage');
 
+
 //global variables for functions
 MerchImage.lastDisplayed = [];
 MerchImage.totalVotes = 0;
 MerchImage.section = document.getElementById('section');
 MerchImage.chartVotes = [];
 MerchImage.chartNames = [];
+MerchImage.thanksHeader = document.getElementById('inputThanks');
 
 function MerchImage(name, url) {
   this.name = name,
   this.url = url,
   this.timesShown = 0,
   this.votes = 0;
-  this.percentShown = 0;
+  this.percentPicked= 0;
 }
 
 var allMerchImages = [
@@ -48,10 +50,12 @@ var allMerchImages = [
   new MerchImage('Wine Glass', 'img/wine-glass.jpg')
 ];
 
-MerchImage.prototype.percentCalc = function () {
-  var ratio = (this.votes / this.timesShown);
-  this.percentShown = Math.round(ratio * 100);
-
+var percentCalc = function () {
+  for (var i = 0; i < allMerchImages.length; i++) {
+    var ratio = (allMerchImages[i].votes / allMerchImages[i].timesShown);
+    allMerchImages[i].percentPicked = Math.round(ratio * 100);
+  }
+  console.log(allMerchImages);
 };
 
 MerchImage.uniqueImageSet = function () {
@@ -76,19 +80,29 @@ MerchImage.displayImages = function () {
   allMerchImages[nestedArray[1]].timesShown++;
   allMerchImages[nestedArray[2]].timesShown++;
 
+
   MerchImage.imgPlaceholderOne.src = allMerchImages[nestedArray[0]].url;
   MerchImage.imgPlaceholderOne.alt = allMerchImages[nestedArray[0]].name;
   MerchImage.imgPlaceholderTwo.src = allMerchImages[nestedArray[1]].url;
   MerchImage.imgPlaceholderTwo.alt = allMerchImages[nestedArray[1]].name;
   MerchImage.imgPlaceholderThree.src = allMerchImages[nestedArray[2]].url;
   MerchImage.imgPlaceholderThree.alt = allMerchImages[nestedArray[2]].name;
+
 };
 
 
 MerchImage.handleClick = function(event) {
   MerchImage.totalVotes++;
-  if(MerchImage.totalVotes >24) {
+  if(MerchImage.totalVotes >4) {
     MerchImage.section.removeEventListener('click', MerchImage.handleClick);
+    MerchImage.imgPlaceholderOne.style.display = 'none';
+    MerchImage.imgPlaceholderTwo.style.display = 'none';
+    MerchImage.imgPlaceholderThree.style.display = 'none';
+    var urDone = document.createTextNode('Thank you for your input!');
+    MerchImage.thanksHeader.appendChild(urDone);
+    percentCalc();
+    MerchImage.voteTotals();
+    MerchImage.renderChart();
   }
   console.log(event.target.alt);
   for(var i in allMerchImages) {
@@ -99,13 +113,45 @@ MerchImage.handleClick = function(event) {
   MerchImage.displayImages();
 };
 
-MerchImage.VoteTotals = function () {
+MerchImage.voteTotals = function () {
   for(var i in allMerchImages) {
-    MerchImage.chartNames[i] = allMerchImages.name[i];
-    MerchImage.chartVotes[i] = allMerchImages.clicks[i];
-  }
-}
+    console.log(allMerchImages[i].name)
+    MerchImage.chartNames[i] = allMerchImages[i].name;
+    MerchImage.chartVotes[i] = allMerchImages[i].clicks;
 
-MerchImage.section.addEventListener('click', MerchImage.handleClick);
+
+  }
+};
+MerchImage.renderChart = function () {
+  var ctx = document.getElementById('chart');
+
+  new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: MerchImage.chartNames,
+      datasets: [{
+        label: 'Total Votes Per Item',
+        data: MerchImage.chartVotes,
+        backgroundColor: 'blue',
+        hoverBackgroundColor: 'red',
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      title: {
+        display: true,
+        text: 'Results',
+      }
+    }
+  });
+};
+
+MerchImage.section.addEventListener ('click', MerchImage.handleClick);
 
 MerchImage.displayImages();
